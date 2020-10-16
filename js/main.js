71682cfa2ccd4d48e78bf8a69a814c62e6714620
KenSerikawa <kenserikawalucio@gmail.com>
2020-10-16 12:40:43 +0200

    Adjust bg size when window is resizing

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@ let c = 9;
 const ordererList = [];
 
 listItems.forEach(element => {
-    console.log(element)
     element.classList.add('animate__animated');
     element.classList.add('animate__bounceInRight');
     element.classList.add('animate__delay-' + c + 'ds');
     c++;
-
 });
+
+jQuery(function($){
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+  
+    $(window).resize(function() {
+      if(windowWidth != $(window).width() || windowHeight != $(window).height()) {
+        location.reload();
+        return;
+      }
+    });
+  });
